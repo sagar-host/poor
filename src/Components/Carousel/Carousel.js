df31efa4c304388547a33a129814e1d87425f0f5
sagar <92850073+sagar-host@users.noreply.github.com>
2022-09-07 22:02:11 +0530

    Update Carousel.js

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -11,10 +11,9 @@ function carousel() {
     <iframe title="heal" width="100%" height='100%' controls className='media'
 src="https://www.youtube.com/embed/0RQWv1ybsjM">
 </iframe>
-    <video width="90%"  className="media">
-      <source src="https://res.cloudinary.com/dcjgktaob/video/upload/v1662501637/ved4_ckfi2m.mp4" />
-      Your browser does not support the video tag.
-    </video>
+       <iframe width="100%" height='100%' controls className='media'
+src="https://www.youtube.com/embed/9EaY11hAod8"  title="Tracker 22">
+</iframe>
     <iframe title="heak" width="100%" height='100%' controls className='media'
 src="https://www.youtube.com/embed/SVcsDDABEkM">
 </iframe>
